11a1afd7024c7f221f317d03972127f9e513b32b
JairoDuenas <jairoduenas.ing@gmail.com>
2022-09-19 15:55:44 -0500

    Ejemplos de estados useState y useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css'
-import Counter from './components/Counter'
+import LoginUseReducer from './components/LoginUseReducer'
+//import LoginUseState from './components/LoginUseState'
+//import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>useReducer Examples</h1>
-        <Counter></Counter>
+        {/* <Counter></Counter> */}
+        {/* <LoginUseState></LoginUseState> */}
+        <LoginUseReducer></LoginUseReducer>
       </header>
     </div>
   );
